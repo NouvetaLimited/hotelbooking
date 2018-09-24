2d1f1d784511e76c4d35e1fb42204bc94e857adf
brianmituka <mitukab@outlook.com>
2018-09-24 09:34:20 +0300

    Added a barchart component

---
+++ src/components/Barchart.vue
@@ -0,0 +1,28 @@
+
+<script>
+import { HorizontalBar } from "vue-chartjs";
+export default {
+  name: "bar-graph",
+  extends: HorizontalBar,
+  props: ["data", "options"],
+  mounted() {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      type: "horizontalBar",
+      labels: [
+        "Tiwi Beach",
+        "Amare Resort",
+        "Ol Pajeta Mansion",
+        "Nairobi Hotel"
+      ],
+      datasets: [
+        {
+          label: "Bookings from each hotel",
+          backgroundColor: ["#F97E3A", "#C8B521", "#FFFF00", "#C12E41"],
+          data: [40, 20, 12, 39]
+        }
+      ]
+    });
+  }
+};
+</script>
