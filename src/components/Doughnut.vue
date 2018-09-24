d6326ceae127b68e3fc46d50be6d25c52d506eb1
brianmituka <mitukab@outlook.com>
2018-09-24 09:36:55 +0300

    Created the donought graph

---
+++ src/components/Doughnut.vue
@@ -0,0 +1,27 @@
+<script>
+import { Doughnut } from "vue-chartjs";
+export default {
+  name: "doughnut-graph",
+  extends: Doughnut,
+  props: ["data"],
+  mounted() {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      type: "horizontalBar",
+      labels: [
+        "Tiwi Beach",
+        "Amare Resort",
+        "Ol Pajeta Mansion",
+        "Nairobi Hotel"
+      ],
+      datasets: [
+        {
+          label: "Bookings from each hotel",
+          backgroundColor: ["#F97E3A", "#C8B521", "#FFFF00", "#C12E41"],
+          data: [40, 20, 12, 39]
+        }
+      ]
+    });
+  }
+};
+</script>
