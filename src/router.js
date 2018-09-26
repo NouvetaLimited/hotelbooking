576e362637545f645ac8401a2cc63a13c9281438
brianmituka <mitukab@outlook.com>
2018-09-26 11:14:30 +0300

    Created routes for different parts of the app

--- src/router.js
+++ src/router.js
@@ -3,15 +3,40 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Dashboard from "@/components/Dashboardhome.vue";
 import Bookings from "@/components/AllBookings.vue";
+import Rooms from "@/components/Rooms.vue";
+import AddHotel from "@/components/AddHotel.vue";
+import AddUser from "@/components/AddUser.vue";
+import RoomType from "@/components/Roomtype.vue";
+import Login from "@/components/Login.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
+      path: "/roomtype",
+      name: "roomtype",
+      component: RoomType
+    },
+    {
       path: "/bookings",
-      name: "all",
+      name: "bookings",
       component: Bookings
+    },
+    {
+      path: "/adduser",
+      name: "adduser",
+      component: AddUser
+    },
+    {
+      path: "/addhotel",
+      name: "addhotel",
+      component: AddHotel
+    },
+    {
+      path: "/rooms",
+      name: "rooms",
+      component: Rooms
     },
     {
       path: "/front",
@@ -20,6 +45,11 @@ export default new Router({
     },
     {
       path: "/",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/dashboard",
       name: "dashboard",
       component: Dashboard
     },
